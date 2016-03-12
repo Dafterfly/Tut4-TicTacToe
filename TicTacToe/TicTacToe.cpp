6b85cc5f632862f7396403f640b983bf091c4c96
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-12 18:38:17 +0200

    Add print() method

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,4 +1,6 @@
 #include "TicTacToe.h"
+#include <string>
+#include <iostream>
 
 
 TicTacToe::TicTacToe()
@@ -12,9 +14,16 @@ TicTacToe::TicTacToe()
 	}
 }
 
-
 TicTacToe::~TicTacToe()
 {
 }
 
-
+void TicTacToe::TicTacToe::print(){
+	int i, j;
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++){
+			cout << "|" << gameBoard[i][j] << "  " << "|";
+		}
+		cout << endl;
+	}
+}
